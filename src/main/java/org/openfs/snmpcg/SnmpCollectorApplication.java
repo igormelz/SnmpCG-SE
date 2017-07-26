767757678a7b6a418fa131fb3de75fe044651827
igormelz <igorm@openfs.ru>
2017-07-26 21:24:31 +0300

    fix for camel 2.19.1

--- src/main/java/org/openfs/snmpcg/SnmpCollectorApplication.java
+++ src/main/java/org/openfs/snmpcg/SnmpCollectorApplication.java
@@ -1,26 +1,24 @@
 package org.openfs.snmpcg;
 
-import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
 import org.apache.camel.spi.ThreadPoolProfile;
-import org.apache.camel.spring.boot.CamelContextConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SnmpCollectorApplication {
 
-	@Value("${snmpcg.poll.threads.min:20}")
+	@Value("${snmpcg.minPoolThreads}")
 	private int poolSize;
 	
-	@Value("${snmpcg.poll.threads.max:40}")
+	@Value("${snmpcg.maxPoolThreads}")
 	private int maxPoolSize;
 	
 	public static void main(String[] args) {
@@ -37,103 +35,99 @@ public class SnmpCollectorApplication {
 		return customProfile;
 	}
 	
-	@Bean
-	public ServletRegistrationBean camelServletRegistrationBean() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(
-				new CamelHttpTransportServlet(), "/api/*");
-		registration.setName("CamelServlet");
-		return registration;
-	}
+	@Component
+	class RestApi extends RouteBuilder {
 
-	@Bean
-	CamelContextConfiguration contextConfiguration() {
-		return new CamelContextConfiguration() {
-
-			@Override
-			public void beforeApplicationStart(CamelContext context) {
-
-				context.setMessageHistory(false);
-				//context.disableJMX();
-
-				if (context.isAllowUseOriginalMessage()) {
-					context.setAllowUseOriginalMessage(false);
-				}
-
-				context.getExecutorServiceManager().setThreadNamePattern("Thread-#counter#");
-			}
-
-			@Override
-			public void afterApplicationStart(CamelContext arg0) {
-				// TODO Auto-generated method stub
-			}
-		};
+		@Override
+		public void configure() {
+			restConfiguration()
+				.contextPath("/api")
+				.bindingMode(RestBindingMode.json);
+			
+			rest("/v1").description("SnmpCG REST service")
+				.get("/sources").description("the list sources")
+					.param().name("status").type(RestParamType.query).endParam()
+					.route().routeId("sources-api")
+					.bean("snmpSources","getSources")
+					.endRest()
+				.get("/sources/{source}").description("source details")
+					.route().routeId("sources-api-details")
+					.bean("snmpSources","getSource(${header.source})")
+					.endRest()
+				.post("/sources/{source}").description("add source")
+					.route().routeId("sources-api-add")	
+					.bean("snmpSources","addSource")
+					.endRest()
+				.delete("/sources/{source}").description("delete source")
+					.route().routeId("sources-api-delete")	
+					.bean("snmpSources","removeSource")
+					.endRest()
+				.get("/sources/{source}/interfaces").description("the list source interfaces")
+					.param().name("trace").type(RestParamType.query).endParam()
+					.param().name("chargeable").type(RestParamType.query).endParam()
+					.route().routeId("sources-api-interfaces")
+					.bean("snmpSources","getSourceInterfaces")
+					.endRest()
+				.put("/sources/{source}/interfaces").description("update interfaces")
+					.param().name("trace").type(RestParamType.query).endParam()
+					.param().name("chargeable").type(RestParamType.query).endParam()
+					.route().routeId("sources-api-update-interfaces")
+					.bean("snmpSources","updateSourceInterface")
+					.endRest()
+				.get("/interfaces").description("the list interfaces")
+					.param().name("trace").type(RestParamType.query).endParam()
+					.param().name("chargeable").type(RestParamType.query).endParam()
+					.route().routeId("interfaces-api")
+					.bean("snmpSources","getInterfaces")
+					.endRest();
+		}
 	}
 
 	@Component
-	class CamelRoutes extends RouteBuilder {
+	class Backend extends RouteBuilder {
 
+		class NullAggregationStrategy implements AggregationStrategy {
+			@Override
+			public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+				return null;
+			}
+		}
+		
 		@Override
-		public void configure() throws Exception {
-
-			restConfiguration().component("servlet").bindingMode(
-					RestBindingMode.json);
-			rest("/v1/")
-					.consumes("application/json")
-					.produces("application/json")
-					
-					.get("/sources/").description("get list sources")
-					.param().name("status").type(RestParamType.query).endParam()
-					.to("bean:sources?method=getSources")
-					
-					.get("/sources/{source}")
-					.to("bean:sources?method=getSource(${header.source})")
-					
-					.post("/sources/{source}")
-					.to("bean:sources?method=addSource")
-					
-					.get("/sources/del/{source}")
-					.to("bean:sources?method=removeSource")
-					
-					.delete("/sources/{source}")
-					.to("bean:sources?method=removeSource")
-					
-					.get("/sources/{source}/interfaces")
-					.param().name("trace").type(RestParamType.query).endParam()
-					.param().name("chargeable").type(RestParamType.query).endParam()
-					.to("bean:sources?method=getSourceInterfaces")
-					
-					.put("/sources/{source}/interfaces")
-					//.param().name("trace").type(RestParamType.query).endParam()
-					//.param().name("chargeable").type(RestParamType.query).endParam()
-					.to("bean:sources?method=updateSourceInterface")
-					
-					.get("/interfaces")
-					.param().name("trace").type(RestParamType.query).endParam()
-					.param().name("chargeable").type(RestParamType.query).endParam()
-					.to("bean:sources?method=getInterfaces")
-					;
-			
+		public void configure() {
 
 			// scheduled poll source status
-			from("timer://validate?period={{snmpcg.validate.period:3m}}").routeId("pollStatus")
-				.split(method("sources", "getDownSources")).parallelProcessing()
-					.bean("snmpService", "pollStatus")
-				.end()
-				;
+			from("timer://validate?period={{snmpcg.validateStatusTimer}}").routeId("pollStatus")
+				.split(method("snmpSources", "getDownSources"),new NullAggregationStrategy()).parallelProcessing()
+					.bean("snmpPoll", "pollStatus")
+				.end();
 			
 			// scheduled poll counters
 			from("quartz2://snmp/poll?cron=0+0/5+*+*+*+?").routeId("pollCounters")
-					.log("start poll sources")
-					.split(method("sources", "getReadySources")).parallelProcessing().executorServiceRef("SnmpCGThreadPoolProfile")
-						.bean("snmpService", "pollCounters")
+				.filter(method("snmpSources","validateStartPoll"))
+					.log("started")
+					.split(method("snmpSources", "getReadySources"),new NullAggregationStrategy()).parallelProcessing().executorServiceRef("SnmpCGThreadPoolProfile")
+						.bean("snmpPoll", "pollCounters")
 					.end()
-					// call external camel route 
-					.to("{{snmpcg.route.flushCounters}}")
-					.to("{{snmpcg.route.flushChargingDataRecord}}")
-					.to("bean:sources?method=setRecoveryState")
-					;
-					
+					.log("${bean:snmpSources?method=logEndPoll}")
+					.bean("snmpSources","setRecoveryState")
+					.to("direct:storeCdr","direct:storeTrace")
+				.end();
+			
+			// store CDR
+			from("direct:storeCdr").routeId("storeCDR")
+				.bean("snmpSources","exportChargingDataRecords")
+				.filter(header("countChargingDataRecords").isGreaterThan(0))
+					.to("{{snmpcg.flushChargingDataRecordEndpoint}}")
+				.end();
+			
+			// store Trace
+			from("direct:storeTrace").routeId("storeTrace")
+				.bean("snmpSources","exportTraceRecords")
+				.filter(header("countTraceRecords").isGreaterThan(0))
+					.to("{{snmpcg.flushTraceCountersEndpoint}}")
+				.end();
 		}
-
 	}
+	
 }
